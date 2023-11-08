22e9c616c098414f50b9fbfa654bf2cc867b631c
donaldumoru <111601390+donaldumoru@users.noreply.github.com>
2023-11-08 10:07:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ let stylesheet = document.getElementById("stylesheet");
 let toggleButton = document.getElementById("toggle-button");
 let showMode = document.getElementById("show-mode");
 
+
+const currentTime = new Date().getHours();
+
+console.log(currentTime)
+
+let counter = 0; // Initialize the counter
+
+if (currentTime >= 17 || currentTime < 8) {
+    stylesheet.setAttribute("href", "css/style.css"); // Set initial state to dark mode for nighttime
+    showMode.textContent = "You are currently viewing my website in dark mode";
+} else {
+    stylesheet.setAttribute("href", "css/lightmode.css"); // Set initial state to light mode for daytime
+    showMode.textContent = "You are currently viewing my website in light mode";
+
+    toggleButton.checked = true; // Set the toggle button to the "on" state
+}
+
 toggleButton.addEventListener("change", switcher);
 
 function switcher() {
@@ -18,3 +35,4 @@ function switcher() {
     }
 
 }
+

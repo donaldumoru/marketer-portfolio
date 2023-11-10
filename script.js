8f19c850dd92cfe86a8620e39f39b0891f70d24d
donaldumoru <111601390+donaldumoru@users.noreply.github.com>
2023-11-10 21:30:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,3 @@
-// // DARK MODE BUTTON
-
-// let stylesheet = document.getElementById("stylesheet");
-// let toggleButton = document.getElementById("toggle-button");
-// let showMode = document.getElementById("show-mode");
-
-
-// const currentTime = new Date().getHours();
-
-// console.log(currentTime)
-
-// let counter = 0; // Initialize the counter
-
-// if (currentTime >= 17 || currentTime < 8) {
-//     stylesheet.setAttribute("href", "css/style.css"); // Set initial state to dark mode for nighttime
-//     showMode.textContent = "You are currently viewing my website in dark mode";
-// } else {
-//     stylesheet.setAttribute("href", "css/lightmode.css"); // Set initial state to light mode for daytime
-//     showMode.textContent = "You are currently viewing my website in light mode";
-
-//     toggleButton.checked = true; // Set the toggle button to the "on" state
-// }
-
-// toggleButton.addEventListener("change", switcher);
-
-// function switcher() {
-//     if (toggleButton.checked) {
-
-//         stylesheet.setAttribute("href", "css/lightmode.css");
-//         showMode.textContent = "You are currently viewing my website in light mode"
-//     } else {
-
-//         stylesheet.setAttribute("href", "css/style.css");
-//         showMode.textContent = "You are currently viewing my website in dark mode"
-//     }
-
-// }
-
-
-
-// let stylesheet = document.getElementById("stylesheet");
-// let toggleButton = document.getElementById("toggle-button");
-// let showMode = document.getElementById("show-mode");
-
-// // Function to set the initial mode based on the user's current time
-// function setInitialModeBasedOnTime() {
-//     const currentTime = new Date().getHours();
-
-//     if (currentTime >= 17 || currentTime < 8) {
-//         stylesheet.setAttribute("href", "css/style.css"); // Set initial mode to dark mode from 17:00 to 8:00
-//         showMode.textContent = "You are currently viewing my website in dark mode";
-//         toggleButton.checked = false;
-//     } else {
-//         stylesheet.setAttribute("href", "css/lightmode.css"); // Set mode to light mode from 8:00 to 16:59
-//         showMode.textContent = "You are currently viewing my website in light mode";
-//         toggleButton.checked = true; // Set the toggle button to the "on" state
-//     }
-// }
-
-// setInitialModeBasedOnTime();
-
-// // Function to set the mode based on the user's preference in storage
-// function setModeFromStorage() {
-//     const mode = sessionStorage.getItem("preferredMode");
-
-//     if (mode === "dark") {
-//         stylesheet.setAttribute("href", "css/style.css");
-//         showMode.textContent = "You are currently viewing my website in dark mode";
-//         toggleButton.checked = false;
-//     } else {
-//         stylesheet.setAttribute("href", "css/lightmode.css");
-//         showMode.textContent = "You are currently viewing my website in light mode";
-//         toggleButton.checked = true;
-//     }
-// }
-
-
-// setModeFromStorage();
-
-
-// toggleButton.addEventListener("change", switcher);
-
-// function switcher() {
-//     if (toggleButton.checked) {
-//         sessionStorage.setItem("preferredMode", "light");
-//         stylesheet.setAttribute("href", "css/lightmode.css");
-//         showMode.textContent = "You are currently viewing my website in light mode";
-//     } else {
-//         sessionStorage.setItem("preferredMode", "dark");
-//         stylesheet.setAttribute("href", "css/style.css");
-//         showMode.textContent = "You are currently viewing my website in dark mode";
-//     }
-
-//     console.log(stylesheet);
-// }
-
-// window.onload = function () {
-
-//     if (document.body.id === "index-page") {
-//         sessionStorage.clear();
-//         setInitialModeBasedOnTime();
-//     };
-
-//     if (document.body.id === "saved-mode") {
-//         // Check if the preferredMode is not already set in localStorage
-//         if (!sessionStorage.getItem("preferredMode")) {
-//             setInitialModeBasedOnTime();
-//         }
-//     }
-// }
-
-
-
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
@@ -153,3 +40,10 @@ function revealBox() {
 window.addEventListener("scroll", revealBox);
 
 revealBox();
+
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}

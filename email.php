4f80a12517095e7fb6a28ca72630365e2a3e93a3
donaldumoru <111601390+donaldumoru@users.noreply.github.com>
2023-11-14 11:32:59 +0100

    Add files via upload

---
+++ email.php
@@ -0,0 +1,35 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 578;
+
+$mail->Username = "dummysomeone8@gmail.com";
+$mail->Password = "qwxo uvgf tlvf vpbw";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("donaldumoru101@gmail.com", "Donald");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: email-sent.html");
